d938a3858b1c11202f939f769213f2e619d12be8
Alice Periles <odahcamperiles@gmail.com>
2021-07-23 16:51:58 -0300

    temos impl \o

--- src/main/java/br/com/alterdata/pack/service/CargoService.java
+++ src/main/java/br/com/alterdata/pack/service/CargoService.java
@@ -3,100 +3,18 @@ package br.com.alterdata.pack.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import br.com.alterdata.pack.exception.BadRequestException;
-import br.com.alterdata.pack.exception.NotFoundException;
 import br.com.alterdata.pack.model.Cargo;
-import br.com.alterdata.pack.repository.CargoRepository;
+
 import br.com.alterdata.pack.shared.CargoDto;
 
-@Service
-public class CargoService {
-    
-    @Autowired
-	private CargoRepository _repositorioCargo;
+public interface CargoService {
 
-    public List<Cargo> obterTodos() {
-	    return this. _repositorioCargo.findAll();
-	}
-
-	public Optional<Cargo> obterPorId(Long id){
-
-        Optional<Cargo> encontrado = _repositorioCargo.findByIdCargo(id);
-
-        if(!encontrado.isPresent()){
-            throw new NotFoundException("Cargo não encontrado pelo ID:" + id);
-            }
-            return encontrado;
-  
-    }
-
-	public List<Cargo> obterPorNome(String nome){
-
-		List<Cargo> cargo = _repositorioCargo.findByNomeContainingIgnoreCase(nome);
-
-		if(cargo.size() == 0){
-		throw new NotFoundException("Não existe cargo com o nome: " + nome);
-		}
-
-		return cargo;
-
-	}
-
-    public Cargo adicionarCargo(Cargo cargo){
-
-        verificarSeCargoExiste(cargo);
-
-        cargo.setIdCargo(null);
-
-        Cargo novoCargo = _repositorioCargo.save(cargo);
-
-        return novoCargo;
-
-    } 
-
-    public Cargo atualizar(Long id, CargoDto cargo) {
-
-        ModelMapper mapper = new ModelMapper();
-
-        Cargo cargoMapeado = mapper.map(cargo, Cargo.class);
-
-        verificarSeCargoExiste(cargoMapeado);
-
-        Optional<Cargo> cargoAtualizado = obterPorId(id);
-
-        if(cargo.getNome() != null){
-            cargoAtualizado.get().setNome(cargo.getNome());
-        }
-        if(cargo.getIcone() != null){
-            cargoAtualizado.get().setIcone(cargo.getIcone());
-        }
-
-        return _repositorioCargo.save(cargoAtualizado.get());
-    }
- 
-    public void deletar(Long id) {
-
-        Optional<Cargo> cargo = obterPorId(id);
-
-        if(cargo.isPresent()){
-            if(cargo.get().getIdCargo() == 1L){
-                throw new BadRequestException("Esse cargo é default e não pode ser apagado :(");
-            }
-        }
-		this._repositorioCargo.deleteById(id);
-	}
-
-    public void verificarSeCargoExiste(Cargo cargo){
-
-        Optional<Cargo> cargoExiste =_repositorioCargo.findByNome(cargo.getNome());
-
-        if(cargoExiste.isPresent()){
-           throw new BadRequestException("Cargo já existe amigo :("); 
-        }
-    }
+    List<Cargo> obterTodos();
+    Optional<Cargo> obterPorId(Long id);
+    List<Cargo> obterPorNome(String nome);
+    Cargo adicionarCargo(Cargo cargo);
+    Cargo atualizar(Long id, CargoDto cargo);
+    void deletar(Long id);
+    void verificarSeCargoExiste(Cargo cargo);
     
 }
--- src/main/java/br/com/alterdata/pack/service/EquipeService.java
+++ src/main/java/br/com/alterdata/pack/service/EquipeService.java
@@ -3,84 +3,14 @@ package br.com.alterdata.pack.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import br.com.alterdata.pack.exception.BadRequestException;
-import br.com.alterdata.pack.exception.NotFoundException;
 import br.com.alterdata.pack.model.Equipe;
-import br.com.alterdata.pack.repository.EquipeRepository;
 
-@Service
-public class EquipeService {
+public interface EquipeService {
 
-    @Autowired
-    private EquipeRepository _repositorioEquipe;
-
-    public List<Equipe> obterTodos() {
-        return this._repositorioEquipe.findAll();
-    }
-
-    public Optional<Equipe> obterPorId(Long id) {
-        Optional<Equipe> encontrado = _repositorioEquipe.findByIdEquipe(id);
-
-        if(!encontrado.isPresent()) {
-            throw new NotFoundException("Não foi encontrado equipe com o ID: " + id);
-        }
-        return encontrado;
-    }
-
-    public List<Equipe> obterPorNome(String nome) {
-        List<Equipe> encontrado = _repositorioEquipe.findByNomeContainingIgnoreCase(nome);
-
-        if (encontrado.size() == 0) {
-            throw new NotFoundException("Não foi encontrado equipe com o nome: " + nome);
-        }
-        return encontrado;
-    }
-
-    public Equipe criarEquipe(Equipe equipe) {
-        equipe.setIdEquipe(null);
-
-        verificarSeEquipeExiste(equipe);
-
-        if (equipe.getNome() == "" || equipe.getNome() == null) {
-            throw new BadRequestException("Nome não pode ser nulo!");
-        }
-        Equipe novoEquipe = _repositorioEquipe.save(equipe);
-        return novoEquipe;
-    }
-    
-    public Equipe atualizar(Long id, Equipe equipe) {
-        equipe.setIdEquipe(id);
-
-        if(equipe.getNome() == "" || equipe.getNome() == null){
-            throw new BadRequestException("Nome não pode ser nulo!");
-        }
-        Equipe equipeAtualizado = _repositorioEquipe.save(equipe);
-        return equipeAtualizado;
-    }
-
-    public void deletar(Long id) {
-
-        Optional<Equipe> equipe = obterPorId(id);
-
-        if(equipe.isPresent()){
-            if(equipe.get().getIdEquipe() == 1L){
-                throw new BadRequestException("Essa equipe é default e não pode ser apagada :(");
-            }
-        }
-        if (!equipe.isPresent()) {
-            throw new NotFoundException("Não existe equipe com o id informado: " + id);
-        }
-        this._repositorioEquipe.deleteById(id);
-    }
-
-    public void verificarSeEquipeExiste(Equipe equipe) {
-        Optional<Equipe> equipeExiste = _repositorioEquipe.findByNome(equipe.getNome());
-
-        if (equipeExiste.isPresent()) {
-            throw new BadRequestException("Opa! Já existe equipe com esse nome.");
-        }
-    }
+    List<Equipe> obterTodos();
+    Optional<Equipe> obterPorId(Long id);
+    List<Equipe> obterPorNome(String nome);
+    Equipe criarEquipe(Equipe equipe);
+    Equipe atualizar(Long id, Equipe equipe);
+    void deletar(Long id);
 }
