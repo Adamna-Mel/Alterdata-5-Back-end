ffbf295d5fc78967fa6c43b03ee44dd8261b6aa1
Adamna-Mel <81425299+Adamna-Mel@users.noreply.github.com>
2021-07-30 17:38:20 -0300

    Update EmailConfig.java

--- src/main/java/br/com/alterdata/pack/config/EmailConfig.java
+++ src/main/java/br/com/alterdata/pack/config/EmailConfig.java
@@ -33,6 +33,7 @@ public class EmailConfig {
 		propriedades.put("mail.smtp.auth", true);
 		propriedades.put("mail.smtp.starttls.enable", true);
 		propriedades.put("mail.smtp.connectiontimeout", 10000);
+		propriedades.put("mail.smtp.ssl.trust", "smtp.gmail.com");
 		
 		mailSender.setJavaMailProperties(propriedades);
 		
